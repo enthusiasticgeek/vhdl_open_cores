3e06845b74a81774fda5f293241b313ab31fe0c7
Pratik Tambe <enthusiasticgeek@gmail.com>
2021-12-12 17:31:06 -0500

    added clk and nrst to signed adder

--- src/adders/signed_n_bitadder.vhd
+++ src/adders/signed_n_bitadder.vhd
@@ -13,6 +13,8 @@ use Work.signed_n_bitadder_package.all;
 entity signed_n_bitadder is
      port (a, b: in std_logic_vector(BUS_WIDTH-1 downto 0);
            c_in : in std_logic;
+	  clk:    in  std_logic;
+	  n_rst:  in  std_logic;
                 sum: out std_logic_vector (BUS_WIDTH-1 downto 0);
                 z, n: out std_logic;
                 c_out, v: out std_logic);
@@ -35,8 +37,15 @@ for I in 0 to BUS_WIDTH-1 generate
            FULL_ADDER0: FULLADDER
                 port map (a(I), b(I), c(I), sum_temp(I), c(I+1));
 end generate;
-           process(c,sum_temp)
+           process(clk,n_rst,c,sum_temp)
            begin
+           if n_rst = '0' then
+             v <='0';
+             n <= '0';
+             z <= '0';
+             c_out <= '0';
+             sum <= (others => '0');
+           elsif rising_edge(clk) then
              v <= c(BUS_WIDTH-1) xor c(BUS_WIDTH);
              c_out <= c(BUS_WIDTH);
              sum <= sum_temp;
@@ -46,5 +55,6 @@ end generate;
              else
                 z <= '0';
              end if;
+           end if;
            end process;
 end architecture;
--- src/adders/signed_n_bitadder_testbench.vhd
+++ src/adders/signed_n_bitadder_testbench.vhd
@@ -12,12 +12,16 @@ architecture testbench of signed_n_bitadder_testbench is
   component signed_n_bitadder
      port (a, b: in std_logic_vector(BUS_WIDTH-1 downto 0);
            c_in : in std_logic;
+	  clk:    in  std_logic;
+	  n_rst:  in  std_logic;
                 sum: out std_logic_vector (BUS_WIDTH-1 downto 0);
                 z, n: out std_logic;
                 c_out, v: out std_logic);
 
   end component;
 
+  signal  clk: std_logic;
+  signal  n_rst: std_logic;
   signal  a,b:  std_logic_vector (BUS_WIDTH-1 downto 0);
   signal  c_in: std_logic;
   signal  sum:  std_logic_vector (BUS_WIDTH-1 downto 0);
@@ -26,6 +30,8 @@ architecture testbench of signed_n_bitadder_testbench is
 
 begin
   dut: signed_n_bitadder port map (
+                            n_rst=>n_rst, 
+                            clk=>clk, 
                             a=>a, 
 			    b=>b, 
 			    c_in=>c_in,
@@ -35,8 +41,27 @@ begin
 			    c_out=>c_out,
                             v=>v
 		    );
+
   process
   begin
+    clk <= '0';
+    wait for 5 ns;
+    clk <= '1';
+    wait for 5 ns;
+  end process;
+
+
+  process
+  begin
+    a <= x"00000000";
+    b <= x"00000000";
+    c_in <= '0';
+
+    n_rst <= '0';
+    wait for 10 ns;
+    n_rst <= '1';
+    wait for 10 ns;
+
     -- addition
     a <= x"FFFF0000";
     b <= x"1000FFFF";
