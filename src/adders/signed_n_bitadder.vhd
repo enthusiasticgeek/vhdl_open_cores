dd0f89ad41c34cd9e77b1a28333b5e11219cd72c
Pratik Tambe <enthusiasticgeek@gmail.com>
2021-12-12 14:38:10 -0500

    signed adder

---
+++ src/adders/signed_n_bitadder.vhd
@@ -0,0 +1,50 @@
+--n bit ripple carry adder
+package signed_n_bitadder_package  is
+  constant BUS_WIDTH : integer := 32;
+  --type input is array (0 to BUS_WIDTH-1) of bit;
+end package;
+
+-- 32-bit adder
+library  ieee;
+use ieee.numeric_std.all;
+use  ieee.std_logic_1164.all;
+use Work.signed_n_bitadder_package.all;
+
+entity signed_n_bitadder is
+     port (a, b: in std_logic_vector(BUS_WIDTH-1 downto 0);
+           c_in : in std_logic;
+                sum: out std_logic_vector (BUS_WIDTH-1 downto 0);
+                z, n: out std_logic;
+                c_out, v: out std_logic);
+end entity;
+
+architecture behaviour of signed_n_bitadder is
+     signal c: signed (BUS_WIDTH downto 0) := (others => '0');
+     signal sum_temp:  std_logic_vector (BUS_WIDTH-1 downto 0) := (others => '0');
+     signal neg: std_logic := '0';
+     --constant zeros : std_logic_vector(c'range-1) := (others => '0');
+     constant zeros : std_logic_vector(BUS_WIDTH-1 downto 0) := (others => '0');
+component FULLADDER
+           port(a, b, c: in std_logic;
+		sum, carry: out std_logic);
+end component;
+begin
+           c(0)<=c_in;
+full_adder:
+for I in 0 to BUS_WIDTH-1 generate
+           FULL_ADDER0: FULLADDER
+                port map (a(I), b(I), c(I), sum_temp(I), c(I+1));
+end generate;
+           process(c,sum_temp)
+           begin
+             v <= c(BUS_WIDTH-1) xor c(BUS_WIDTH);
+             c_out <= c(BUS_WIDTH);
+             sum <= sum_temp;
+             n <= sum_temp(BUS_WIDTH-1);
+             if sum_temp = zeros then
+                z <= '1';
+             else
+                z <= '0';
+             end if;
+           end process;
+end architecture;
---
+++ src/adders/signed_n_bitadder_testbench.vhd
@@ -0,0 +1,72 @@
+-- n bit ripple carry Adder Testbench
+--https://github.com/enthusiasticgeek/vhdl_opesigned_n_cores
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use Work.signed_n_bitadder_package.all;
+
+entity signed_n_bitadder_testbench is
+end entity;
+
+architecture testbench of signed_n_bitadder_testbench is
+  component signed_n_bitadder
+     port (a, b: in std_logic_vector(BUS_WIDTH-1 downto 0);
+           c_in : in std_logic;
+                sum: out std_logic_vector (BUS_WIDTH-1 downto 0);
+                z, n: out std_logic;
+                c_out, v: out std_logic);
+
+  end component;
+
+  signal  a,b:  std_logic_vector (BUS_WIDTH-1 downto 0);
+  signal  c_in: std_logic;
+  signal  sum:  std_logic_vector (BUS_WIDTH-1 downto 0);
+  signal  z, n: std_logic;
+  signal  c_out,v: std_logic;
+
+begin
+  dut: signed_n_bitadder port map (
+                            a=>a, 
+			    b=>b, 
+			    c_in=>c_in,
+                            sum=>sum, 
+                            z=>z,
+                            n=>n,
+			    c_out=>c_out,
+                            v=>v
+		    );
+  process
+  begin
+    -- addition
+    a <= x"FFFF0000";
+    b <= x"1000FFFF";
+    c_in <= '0';
+    wait for 10 ns;
+    -- addition
+    a <= x"01000000";
+    b <= x"1000FFFF";
+    c_in <= '0';
+    wait for 10 ns;
+    -- addition
+    a <= x"00000000";
+    b <= x"00000000";
+    c_in <= '0';
+    wait for 10 ns;
+    -- addition
+    a <= x"FFFFFFFF";
+    b <= x"FFFFFFFF";
+    c_in <= '1';
+    wait for 10 ns;
+    -- subtraction
+    a <= x"FFFFFFFF";
+    b <= x"00000001"; -- b is 2's complement of a
+    c_in <= '0';
+    wait for 10 ns;
+    -- subtraction
+    a <= x"000000FF";
+    b <= x"FFFFFF01"; -- b is 2's complement of a
+    c_in <= '0';
+    wait for 10 ns;
+    wait;
+  end process;
+end;
